0234a997f629873ca5be4ee866b97808fabc361d
JesseJohn7 <Jessejohn260@gmail.com>
2026-06-01 15:28:26 +0100

    feat: build @iln/indexer Horizon event indexer utility

---
+++ packages/indexer/src/index.ts
@@ -0,0 +1,10 @@
+export { ILNEventIndexer } from "./indexer";
+export type {
+  ContractEvent,
+  EventCallback,
+  ILNEventType,
+  IndexerOptions,
+  SubscriptionHandle,
+} from "./types";
+export { parseContractEvent } from "./parse";
+export type { RawHorizonEvent } from "./parse";
